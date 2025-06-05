471806831f4937182846f4f4428f4308f5a836b0
MisterNorse <moranh12899@gmail.com>
2025-06-05 02:38:17 -0400

    Update storyScenes.ts

--- data/storyScenes.ts
+++ data/storyScenes.ts
@@ -2,7 +2,7 @@ import { StoryScene, ItemType } from "@/types/game";
 import { getCharacterPortrait } from "@/data/characterPortraits";
 
 export const storyScenes: StoryScene[] = [
-  // Prologue - The Nightmares Begin
+  // Prologue - New Beginnings
   {
     id: "prologue_1",
     background: "https://images.unsplash.com/photo-1518709268805-4e9042af9f23?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFudGFzeSUyMHZpbGxhZ2V8ZW58MHx8MHx8fDA%3D",
